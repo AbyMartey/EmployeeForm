a9caaf1a3d5b7aaf750e911a8c33f2244d6004ae
Abigail Martey <77507407+AbyMartey@users.noreply.github.com>
2022-09-16 11:18:22 +0000

    Add files via upload

---
+++ EmployeeForm/employeeForm.js
@@ -0,0 +1,9 @@
+const submitButton=document.getElementById("submitbutton");
+
+function clickSubmit(){
+    alert("Thank yor for filling the form");
+}
+
+submitButton.addEventListener("click",function(){
+clickSubmit();
+});
